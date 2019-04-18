897ef18778aba593835b7b0b881c8d944c42e329
root <1514324415@qq.com>
2019-04-18 11:28:47 +0800

    Junit测试reids发布和订阅

---
+++ src/main/java/com/redis/junit/RedisSub.java
@@ -0,0 +1,33 @@
+package com.redis.junit;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+
+/**
+ * @Author: jyh
+ * @Date: 2019/4/15 20:40
+ */
+public class RedisSub {
+
+    private Logger logger = LoggerFactory.getLogger(RedisPub.class);
+
+    @Test
+    public void sub() {
+        logger.info("------订阅者------");
+        Jedis jedis = null;
+        try {
+            jedis = new Jedis("127.0.0.1", 6379);
+            RedisPubSub redisPubSub = new RedisPubSub();
+            jedis.subscribe(redisPubSub, "channelOne", "channelTwo");
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(jedis != null) {
+                jedis.disconnect();
+            }
+        }
+    }
+
+}
