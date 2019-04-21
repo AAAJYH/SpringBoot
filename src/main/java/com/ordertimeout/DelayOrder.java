b7a8637b6cc5036775b4088c0863841ac34909c3
root <1514324415@qq.com>
2019-04-21 20:46:49 +0800

    订单超时处理：延时队列来实现

--- src/main/java/com/ordertimeout/DelayOrder.java
+++ src/main/java/com/ordertimeout/DelayOrder.java
@@ -3,7 +3,6 @@ package com.ordertimeout;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Date;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
@@ -15,26 +14,28 @@ import java.util.concurrent.TimeUnit;
  */
 
 @Data
-public class DelayOrder implements Delayed{
+public class DelayOrder extends Order implements Delayed{
 
-    private int orderId;
-    private Date orderTime;
-    private Long expireTime = 5000L;
+    Logger logger = LoggerFactory.getLogger(DelayOrder.class);
 
+    /** 超时毫秒 */
+    private final Long expireTime = 5000L;
+
+    /** 初始化延时队列 */
     public static DelayQueue<DelayOrder> delayQueue;
     static {
-        new DelayQueue<>();
+        delayQueue = new DelayQueue<DelayOrder>();
     }
 
     @Override
     public long getDelay(TimeUnit unit) {
-        System.out.println(orderTime.getTime() + expireTime - System.currentTimeMillis()+ "毫秒");
-        return  this.orderTime.getTime() + expireTime - System.currentTimeMillis();
+        logger.info(this.getOrderTime() + expireTime - System.currentTimeMillis()+ "毫秒");
+        return  this.getOrderTime() + expireTime - System.currentTimeMillis();
     }
 
     @Override
     public int compareTo(Delayed o) {
-        return this.orderTime.getTime() - ((DelayOrder) o).orderTime.getTime() > 0 ? 1 : 0;
+        return this.getOrderTime() - ((DelayOrder) o).getOrderTime() > 0 ? 1 : 0;
     }
 
     public static void main(String[] args) {
@@ -42,33 +43,35 @@ public class DelayOrder implements Delayed{
 
         /** 订单1 */
         DelayOrder delayOrder = new DelayOrder();
-        delayOrder.setOrderTime(new Date());
+        delayOrder.setOrderTime(System.currentTimeMillis());
         delayOrder.setOrderId(1);
         /** 加入到延时队列中 */
         delayQueue.offer(delayOrder);
-        System.out.println(delayOrder.getOrderId()+ "订单1加入时间："+ delayOrder.getOrderTime());
+        logger.info(delayOrder.getOrderId()+ "订单1加入时间："+ delayOrder.getOrderTime());
 
         /** 订单2 */
         DelayOrder delayOrder2 = new DelayOrder();
-        delayOrder2.setOrderTime(new Date());
+        delayOrder2.setOrderTime(System.currentTimeMillis());
         delayOrder2.setOrderId(2);
         /** 加入到延时队列中 */
         delayQueue.offer(delayOrder2);
-        System.out.println(delayOrder2.getOrderId()+ "订单2加入时间："+ delayOrder2.getOrderTime());
+        logger.info(delayOrder2.getOrderId()+ "订单2加入时间："+ delayOrder2.getOrderTime());
 
         /** 启动线程 */
         consum();
     }
 
     private static void consum() {
-            while(true) {
+        Logger logger = LoggerFactory.getLogger(DelayOrder.class);
+
+        while(true) {
                 try {
 
                     /** take()：底层取队列中第一个元素循环去判断getDelay是否小于零（是否超时）
                      *  第一个元素处理完后移除，下一个元素向前移位，DelayQueue为空时线程等待
                      */
                     DelayOrder delayOrder = delayQueue.take();
-                    System.out.println("现在时间是："+ delayOrder.getOrderTime()+ "，订单过期："+ delayOrder.getOrderId());
+                    logger.info("现在时间是："+ delayOrder.getOrderTime()+ "，订单过期："+ delayOrder.getOrderId());
 
                     /**
                      * @TODO 订单超时，修改订单状态
