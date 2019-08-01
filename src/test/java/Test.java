607c454499f22262ac9e52131a3a948a30712acc
jiyuhang <1514324415@qq.com>
2019-08-01 21:07:01 +0800

    测试

---
+++ src/test/java/Test.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * @author: 姬雨航
+ * @date: 2019/7/29 11:32
+ * @description：
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        String a = "magnet:?xt=urn:btih:";
+        String b = "0002405ceeb0de28e3e05ddc901038081a36d99e";
+        System.out.println(a+b);
+    }
+
+}
---
+++ src/test/java/mock/MockTest.java
@@ -0,0 +1,17 @@
+package mock;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author: 姬雨航
+ * @date: 2019/7/29 14:05
+ * @description：
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockTest {
+}
