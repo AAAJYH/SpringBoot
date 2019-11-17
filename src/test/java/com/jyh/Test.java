05d211a56188829c0a958dcb69a0c99bce104a50
root <1514324415@qq.com>
2019-11-17 19:30:18 +0800

    jyh

--- src/test/java/com/jyh/Test.java
+++ src/test/java/com/jyh/Test.java
@@ -1,7 +1,17 @@
 package com.jyh;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.jyh.反射.Person;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.json.JSONObject;
+import org.json.JSONString;
+import springfox.documentation.spring.web.json.Json;
+import sun.security.provider.MD5;
+
+import javax.tools.Tool;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author: 姬雨航
@@ -12,10 +22,37 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        Integer a = 1000;
-        Integer b = 1000;
 
-        System.out.println(a == b);
+
+        System.out.println(Test.md5Password("123"));
+    }
+
+    public static String md5Password(String password) {
+
+        try {
+            // 得到一个信息摘要器
+            MessageDigest digest = MessageDigest.getInstance("md5");
+            byte[] result = digest.digest(password.getBytes());
+            StringBuffer buffer = new StringBuffer();
+            // 把每一个byte 做一个与运算 0xff;
+            for (byte b : result) {
+                // 与运算
+                int number = b & 0xff;// 加盐
+                String str = Integer.toHexString(number);
+                if (str.length() == 1) {
+                    buffer.append("0");
+                }
+                buffer.append(str);
+            }
+
+            // 标准的md5加密后的结果
+            return buffer.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+
+            return "";
+        }
+
     }
 
 }
--- src/test/java/com/jyh/反射/Person.java
+++ src/test/java/com/jyh/反射/Person.java
@@ -1,5 +1,10 @@
 package com.jyh.反射;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class Person {
 
     String name;
